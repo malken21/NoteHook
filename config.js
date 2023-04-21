9d0a5e4fd6be176d7e990c6441b27ea8cfd02261
malken21 <Malken_Marumasa@outlook.jp>
2023-04-22 01:17:06 +0900

    update

--- config.js
+++ config.js
@@ -1,3 +1,4 @@
 const Discord_Webhook = "Discord Webhook URL";
-const Message = "ノートが作成されました https://misskey.io/notes/%note_id%";
+const Instance = "misskey.io"
+const Message = "ノートが作成されました https://%Instance%/notes/%note_id%";
 const Color = 8827648;
--- util.js
+++ util.js
@@ -9,12 +9,12 @@ function result(status) {
 function sendDiscord(note) {
 
     const payload = {
-        "content": Message.replace("%note_id%", note.id),
+        "content": Message.replace("%note_id%", note.id).replace("%Instance%", Instance),
         "embeds": [
             {
                 "title": `${note.user.name} (@${note.user.username})`,
                 "description": note.text,
-                "url": `https://misskey.io/notes/${note.id}`,
+                "url": `https://${Instance}/notes/${note.id}`,
                 "color": Color,
                 "thumbnail": {
                     "url": note.user.avatarUrl
