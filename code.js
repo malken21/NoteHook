24284fbd254d59978f8a8707a1fc5845b99c357a
malken21 <Malken_Marumasa@outlook.jp>
2023-04-21 19:44:13 +0900

    update

--- code.js
+++ code.js
@@ -5,6 +5,10 @@ function doPost(e) {
         const data = JSON.parse(e.postData.contents);
         //JSONをコンソール出力
         console.log(data);
+
+        //一時停止
+        Utilities.sleep(SleepTime);
+
         // Discordに送信
         sendDiscord(Message.replace("%note_id%", data.body.note.id));
         //レスポンス
--- config.js
+++ config.js
@@ -1,2 +1,3 @@
 const Discord_Webhook = "Discord Webhook URL";
 const Message = "ノートが作成されました https://misskey.io/notes/%note_id%";
+const SleepTime = 30000;
