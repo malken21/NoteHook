ce33cf14c68a848a505133b2768dc435d743306c
malken21 <Malken_Marumasa@outlook.jp>
2023-04-22 01:19:22 +0900

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 const Discord_Webhook = "Discord Webhook URL";
-const Instance = "misskey.io"
+const Instance = "misskey.io";
 const Message = "ノートが作成されました https://%Instance%/notes/%note_id%";
 const Color = 8827648;
