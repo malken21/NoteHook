86dc6241e9d8833b03bcbe6f1f76b7ff8fec7202
malken21 <Malken_Marumasa@outlook.jp>
2023-06-06 02:06:51 +0900

    Revert "update"
    This reverts commit ce110e806fd3c8e9f9ea98a7ed607a8f6b173693.

--- code.js
+++ code.js
@@ -38,30 +38,20 @@ function run() {
     // プロジェクトプロパティ取得
     const properties = PropertiesService.getScriptProperties();
 
-    // プロジェクトプロパティの"TriggerData"のデータを取得
-    const idList = JSON.parse(properties.getProperty("TriggerData"));
-    // プロジェクトプロパティの"MessageData"のデータを取得
-    const textList = JSON.parse(properties.getProperty("MessageData"));
+    // 一番上にあるプロジェクトプロパティを取得
+    const id = properties.getKeys().slice(-1)[0];
+    const text = properties.getProperty(id);
+    // プロジェクトプロパティ削除
+    properties.deleteProperty(id);
 
-    // "idList"の先頭の要素を削除、削除したものを"id"に代入
-    const id = idList.shift();
-    // "textList"の先頭の要素を削除、削除したものを"text"に代入
-    const text = textList.shift();
-
-    // プロジェクトプロパティの"TriggerData"のデータを設定
-    properties.setProperty("TriggerData", JSON.stringify(idList));
-
-    // プロジェクトプロパティの"MessageData"のデータを設定
-    properties.setProperty("MessageData", JSON.stringify(textList));
+    // Discordに送信
+    sendDiscord(text);
+    // Discordに送信する文字をログ出力
+    console.log(text);
 
     // トリガーを削除
     ScriptApp.getProjectTriggers().forEach(trigger => {
         if (trigger.getUniqueId() == id)
             ScriptApp.deleteTrigger(trigger);
     });
-
-    // Discordに送信
-    sendDiscord(text);
-    // Discordに送信する文字をログ出力
-    console.log(text);
 }
